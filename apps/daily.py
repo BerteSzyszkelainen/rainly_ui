76e00b291c20c4ee83000ab257a089026144a320
szysz.bartlomiej <szysz.bartlomiej@gmail.com>
2022-01-04 18:32:53 +0100

    Base heatmap chart

--- apps/daily.py
+++ apps/daily.py
@@ -3,8 +3,11 @@ from dash import dcc
 from dash import html
 from datetime import datetime
 from dash.dependencies import Input, Output
+import plotly.express as px
 
 from app import app
+
+BACKGROUND_COLOR = "#5D5C61"
 
 layout = html.Div(
     id="root-div",
@@ -23,15 +26,42 @@ layout = html.Div(
             ]
         ),
         html.Div(
-            id="div-welcome",
-            children=html.Label(id='label-welcome', children="W budowie:)")
+            id="div-heatmap-chart",
+            children=dcc.Graph(id="heatmap-chart")
         ),
         dcc.Interval(
             id='interval-timer',
             interval=1 * 1000,
             n_intervals=0
+        ),
+        dcc.Interval(
+            id='interval-measurement',
+            interval=60 * 1000,
+            n_intervals=0
         )
 ])
+
+@app.callback(
+    Output(component_id='heatmap-chart', component_property='figure'),
+    Input(component_id='interval-measurement', component_property='n_intervals')
+)
+def update_bar_chart(n):
+
+    fig = px.imshow([[1, 20, 30],
+                     [20, 1, 60],
+                     [30, 60, 1]])
+
+    fig.update_layout(plot_bgcolor=BACKGROUND_COLOR)
+    fig.update_layout(paper_bgcolor=BACKGROUND_COLOR)
+    fig.update_layout(font={"color": "white", "size": 20})
+    fig.update_layout(
+        hoverlabel=dict(
+            font_size=32,
+            font_family="Lucida Console"
+        )
+    )
+
+    return fig
 
 
 @app.callback(
