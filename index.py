d8ce84e023b765ce5c5a1e34ee56e2f220369e0f
szysz.bartlomiej <szysz.bartlomiej@gmail.com>
2022-01-05 12:21:16 +0100

    Fix problem with app start

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 
-from app import app
+from app import app, server
 from apps import daily, yearly, home, monthly
 
 app.layout = html.Div([
